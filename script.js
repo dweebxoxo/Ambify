181610998616b24f173e974b738dbcd1c35aa823
Rohit <rvbamble@jspmrscoe.edu.in>
2023-05-04 21:21:30 +0530

    All audio variables, functions, loops declared, working

--- script.js
+++ script.js
@@ -58,17 +58,77 @@ function playpause4() {
     }
 }
 
-// var sound5 = new Audio();
-// sound5.src = "Task-Audios/Audios/city_road.wav"
+var sound5 = new Audio();
+sound5.src = "/Task-Audios/Audios/city_road.wav"
 
-// var sound6 = new Audio();
-// sound6.src = "Task-Audios/Audios/clock_timer.wav"
+var PlayPauseBTN5 = document.getElementById('PlayPauseBTN5');
+var count = 0;
+function playpause5() {
+    if (count == 0) {
+        count = 1;
+        sound5.play();
+    } else {
+        count = 0;
+        sound5.pause();
+    }
+}
 
-// var sound7 = new Audio();
-// sound7.src = "Task-Audios/Audios/rain.mp3"
+var sound6 = new Audio();
+sound6.src = "/Task-Audios/Audios/clock_timer.wav"
 
-// var sound8 = new Audio();
-// sound8.src = "Task-Audios/Audios/water_waves.wav"
+var PlayPauseBTN6 = document.getElementById('PlayPauseBTN6');
+var count = 0;
+function playpause6() {
+    if (count == 0) {
+        count = 1;
+        sound6.play();
+    } else {
+        count = 0;
+        sound6.pause();
+    }
+}
 
-// var sound9 = new Audio();
-// sound9.src = "Task-Audios/Audios/wind.wav"
+var sound7 = new Audio();
+sound7.src = "/Task-Audios/Audios/rain.mp3"
+
+var PlayPauseBTN7 = document.getElementById('PlayPauseBTN7');
+var count = 0;
+function playpause7() {
+    if (count == 0) {
+        count = 1;
+        sound7.play();
+    } else {
+        count = 0;
+        sound7.pause();
+    }
+}
+
+var sound8 = new Audio();
+sound8.src = "/Task-Audios/Audios/water_waves.wav"
+
+var PlayPauseBTN8 = document.getElementById('PlayPauseBTN8');
+var count = 0;
+function playpause8() {
+    if (count == 0) {
+        count = 1;
+        sound8.play();
+    } else {
+        count = 0;
+        sound8.pause();
+    }
+}
+
+var sound9 = new Audio();
+sound9.src = "/Task-Audios/Audios/wind.wav"
+
+var PlayPauseBTN9 = document.getElementById('PlayPauseBTN9');
+var count = 0;
+function playpause9() {
+    if (count == 0) {
+        count = 1;
+        sound9.play();
+    } else {
+        count = 0;
+        sound9.pause();
+    }
+}
